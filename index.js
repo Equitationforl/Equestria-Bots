d433a5101a58d98cae12ede38f29a03570ad3457
Equitationforl <46785539+Equitationforl@users.noreply.github.com>
2019-01-17 22:23:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg require('./index.json'); // a garder en version desktop
-const token = process.env.token // a garder en version heroku
 const prefix = ("?");
 
 bot.on('ready', function () {
